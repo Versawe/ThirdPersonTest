bce37b8e6fc4b5a6b196f66eebd8e0005d4492c0
Eric Versaw <ericversaw22@gmail.com>
2021-02-15 21:25:17 -0500

    Got started on a third person controller for fun

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    private Camera cam;
+
+    private float yawSensitivity = 8.5f;
+    private float pitchSensitivity = 5.5f;
+
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = target.position;
+
+        RotateCamera();
+    }
+
+    private void RotateCamera()
+    {
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
+
+        yaw += mx * yawSensitivity;
+        pitch -= my * pitchSensitivity;
+
+        float pitch_clamped = Mathf.Clamp(pitch, -45f, 45f);
+
+        transform.rotation = Quaternion.Euler(pitch_clamped, yaw, 0);
+    }
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private CharacterController cc;
+
+    private float playerSpeed = 5;
+
+    public Transform cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+
+        float h = Input.GetAxisRaw("Horizontal");
+
+        Vector3 inputDir = transform.forward * v + transform.right * h;
+
+        cc.SimpleMove(inputDir * playerSpeed);
+
+        if (h != 0 || v != 0)
+        {
+            //transform.forward = cam.forward;
+
+            Quaternion camRot = Quaternion.Euler(0, cam.eulerAngles.y, 0);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, camRot, 0.1f);
+        }
+    }
+}
