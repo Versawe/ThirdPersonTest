27230b9a21c17e6ce37e9556f772fc26562304c5
Eric Versaw <ericversaw22@gmail.com>
2021-02-16 19:20:29 -0500

    Added collider physics to character controller
    player can move tiny square now and started to work on a skidding mechanic so the movement feels more fluid. Once complete with that I should copy the Scene then change it and add scripts to make a fps version.

--- Assets/Scripts/CharacterMovementThirdPerson.cs
+++ Assets/Scripts/CharacterMovementThirdPerson.cs
@@ -11,10 +11,14 @@ public class CharacterMovementThirdPerson : MonoBehaviour
     private bool jumpOnce = false;
 
     Vector3 inputDir;
+    Vector3 copyInputVec;
+    private bool isMoving = false;
 
     private float jumpSpeed = 8.5f;
     private float gravity = 20f;
     private Vector3 moveDirection = Vector3.zero;
+
+    private float pushPower = 2f;
 
     public Transform cam;
     // Start is called before the first frame update
@@ -30,36 +34,74 @@ public class CharacterMovementThirdPerson : MonoBehaviour
         JumpPlayer();
     }
 
+    //movement for player
     private void MovePlayer()
     {
+        //using axis raw to allow for wasd direction to be stored in Vector3 
         float v = Input.GetAxisRaw("Vertical");
-
         float h = Input.GetAxisRaw("Horizontal");
-
         inputDir = transform.forward * v + transform.right * h;
 
+        //applies input vector to apply to char controller
         cc.SimpleMove(inputDir * playerSpeed);
 
-
+        //this statement rotates the character to be facing away from camera if movement is applied
         if (h != 0 || v != 0)
         {
-            //transform.forward = cam.forward;
 
             Quaternion camRot = Quaternion.Euler(0, cam.eulerAngles.y, 0);
 
             transform.rotation = Quaternion.Lerp(transform.rotation, camRot, 0.1f);
+
+            isMoving = true;
         }
+        if (isMoving && h != 0 || v != 0)
+        {
+            copyInputVec = inputDir;
+            //Vector3.Lerp(inputDir, new Vector3(0,0,0), 0.01f);
+            //print("hello");
+        }
+        else
+        {
+            isMoving = false;
+        }
+        //ON THE RIGHT TRACK FINISH AT KBELLS!! GOOD LUCK!
+        print(copyInputVec);
     }
 
+    //player jump ability
     private void JumpPlayer()
     {
+        //checks if spacebar clicked and only triggable once
         if (!jumpOnce && Input.GetButton("Jump"))
         {
             moveDirection.y = jumpSpeed;
             jumpOnce = true;
         }
+        //applies gravity and move ability for char controller
         moveDirection.y -= gravity * Time.deltaTime;
         cc.Move(moveDirection * Time.deltaTime);
+
+        //if on ground locks jumponce to be sure to allow character to jump again
         if (cc.isGrounded) jumpOnce = false;
+    }
+
+    //pushes block
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody hitBody = hit.collider.attachedRigidbody;
+
+        if(hitBody == null || hitBody.isKinematic)
+        {
+            return;
+        }
+
+        if (hit.moveDirection.y < -0.3f)
+        {
+            return;
+        }
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        hitBody.velocity = pushDir * pushPower;
     }
 }
