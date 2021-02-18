d30db00d80f5fd3b16af9c920392c7f4ac2ad0d8
Eric Versaw <ericversaw22@gmail.com>
2021-02-17 19:00:26 -0500

    Done with fps controller
    easy switch over with a lot changed to better fit a fps game. I like these two setups a lot and think they would be good for a starter controller for future games if needed, and i better understand them since I wrote them, which is a plus.

---
+++ Assets/Scripts/CameraRotationFirstPerson.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotationFirstPerson : MonoBehaviour
+{
+    private Camera cam;
+
+    private float yawSensitivity = 9.5f;
+    private float pitchSensitivity = 6.5f;
+
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    public Transform target;
+
+    private Animator an;
+    private bool run;
+    private bool left;
+    private bool right;
+    private bool idle;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // gets main camera
+        cam = Camera.main;
+
+        player = GameObject.Find("Player");
+
+        an = player.GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //has rig set to focus on character
+        Vector3 targetPositionFixed = new Vector3(target.position.x, target.position.y +0.4f, target.position.z);
+        transform.position = targetPositionFixed;
+
+        RotateCamera();
+    }
+
+    private void RotateCamera()
+    {
+        //saves axis movement of x and y mouse movement
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
+
+        // yaw and pitch values change determined on 
+        // mousex and mousey movement and applied sensitivity to both
+        yaw += mx * yawSensitivity;
+        pitch -= my * pitchSensitivity;
+
+        //clamp pitch, so camera doesn't rotate too far low or high to seem weird
+        float pitch_clamped = Mathf.Clamp(pitch, -89f, 45f);
+
+        // use the clamped pitch and yaw to rotate camera rig, entered in as euler angles through Quaternion class 
+        transform.rotation = Quaternion.Euler(pitch_clamped, yaw, 0);
+
+        if (mx != 0 && !player.GetComponent<CharacterMovementFirstPerson>().isMoving)
+        {
+            run = true;
+            left = false;
+            right = false;
+            idle = false;
+
+            an.SetBool("IsRun", run);
+            an.SetBool("IsLeft", left);
+            an.SetBool("IsRight", right);
+            an.SetBool("IsIdle", idle);
+        }
+        if (mx == 0 && !player.GetComponent<CharacterMovementFirstPerson>().isMoving)
+        {
+            run = false;
+            left = false;
+            right = false;
+            idle = true;
+
+            an.SetBool("IsRun", run);
+            an.SetBool("IsLeft", left);
+            an.SetBool("IsRight", right);
+            an.SetBool("IsIdle", idle);
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterMovementFirstPerson.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementFirstPerson : MonoBehaviour
+{
+    private CharacterController cc;
+    private Animator an;
+
+    private float playerSpeed = 8f;
+
+    Vector3 inputDir;
+    Vector3 copyInputVec;
+    public bool isMoving = false;
+    private float skidDelay = 0.15f;
+    private float skidLength = 0.75f;
+
+    private float jumpSpeed = 9f;
+    private float gravity = 20f;
+    private Vector3 moveDirection = Vector3.zero;
+    private bool jumpOnce = false;
+
+    private float pushPower = 2f;
+
+    private bool run;
+    private bool left;
+    private bool right;
+    private bool idle;
+
+    public Transform cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        an = GetComponentInChildren<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        JumpPlayer();
+    }
+
+    //movement for player
+    private void MovePlayer()
+    {
+        //gets input from WASD using Unity's Vertical and Horizontal axis's
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        //applies this gameobjects forward and right vector to a vector 3 depending on the axis's numbers
+        // Example: w = 1, s = -1, a = -1, d = 1
+        inputDir = transform.forward * v + transform.right * h;
+
+        //Animation Triggers below, should come up with a function to shorten the length of each statement in the FUTURE
+        //animations for running, forward, left and right
+        if (v != 0) //forward
+        {
+            run = true;
+            left = false;
+            right = false;
+            idle = false;
+
+            an.SetBool("IsRun", run);
+            an.SetBool("IsLeft", left);
+            an.SetBool("IsRight", right);
+            an.SetBool("IsIdle", idle);
+        }
+        else if (h < 0) // left
+        {
+            run = false;
+            left = true;
+            right = false;
+            idle = false;
+
+            an.SetBool("IsRun", run);
+            an.SetBool("IsLeft", left);
+            an.SetBool("IsRight", right);
+            an.SetBool("IsIdle", idle);
+        }
+        else if (h > 0) // right
+        {
+            run = false;
+            left = false;
+            right = true;
+            idle = false;
+
+            an.SetBool("IsRun", run);
+            an.SetBool("IsLeft", left);
+            an.SetBool("IsRight", right);
+            an.SetBool("IsIdle", idle);
+        }
+        else if(h > 0 || h < 0 && v !=0) // angle aka forward
+        {
+            run = true;
+            left = false;
+            right = false;
+            idle = false;
+
+            an.SetBool("IsRun", run);
+            an.SetBool("IsLeft", left);
+            an.SetBool("IsRight", right);
+            an.SetBool("IsIdle", idle);
+        }
+        //animation for idle
+        if (h == 0 && v == 0)
+        {
+            run = false;
+            left = false;
+            right = false;
+            idle = true;
+
+            an.SetBool("IsRun", run);
+            an.SetBool("IsLeft", left);
+            an.SetBool("IsRight", right);
+            an.SetBool("IsIdle", idle);
+        }
+
+        //skidding functionality
+        if (isMoving)
+        {
+            if (h != 0 || v != 0)
+            {
+                copyInputVec = inputDir;
+            }
+            else
+            {
+                inputDir = Vector3.Lerp(copyInputVec, new Vector3(0, 0, 0), skidLength);
+                skidDelay -= Time.deltaTime;
+                if (skidDelay <= 0) isMoving = false;
+            }
+        }
+        if (!isMoving)
+        {
+            skidDelay = 0.15f;
+        }
+
+        //applies input vector to apply to char controller
+        cc.SimpleMove(inputDir * playerSpeed);
+
+        //this statement rotates the character to be facing away from camera if movement is applied
+        //CHANGED FOR FPS   
+        Quaternion camRot = Quaternion.Euler(0, cam.eulerAngles.y, 0);
+        transform.rotation = camRot;
+        if (h != 0 || v != 0)
+        {
+            isMoving = true;
+        }
+    }
+
+    //player jump ability
+    private void JumpPlayer()
+    {
+        //checks if spacebar clicked and only triggable once
+        if (!jumpOnce && Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpSpeed;
+            jumpOnce = true;
+        }
+        //applies gravity and move ability for char controller
+        moveDirection.y -= gravity * Time.deltaTime;
+        cc.Move(moveDirection * Time.deltaTime);
+
+        //if on ground locks jumponce to be sure to allow character to jump again
+        if (cc.isGrounded) jumpOnce = false;
+    }
+
+    //pushes block
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody hitBody = hit.collider.attachedRigidbody;
+
+        if(hitBody == null || hitBody.isKinematic)
+        {
+            return;
+        }
+
+        if (hit.moveDirection.y < -0.3f)
+        {
+            return;
+        }
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        hitBody.velocity = pushDir * pushPower;
+    }
+}
